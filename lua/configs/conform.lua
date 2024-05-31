986425cc5781b8260916efbeffbbbae032c6f3cb
Treast <vincent.riva@outlook.fr>
2024-05-31 18:37:32 +0200

    Fix format on save

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,25 @@
 local options = {
   formatters_by_ft = {
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    svelte = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    json = { "prettier" },
+    yaml = { "prettier" },
+    markdown = { "prettier" },
+    graphql = { "prettier" },
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    php = { "php-cs-fixer" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(options)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
@@ -18,7 +18,7 @@ return {
   opts = {
     ensure_installed = {
       "lua-language-server", "stylua",
-      "html-lsp", "css-lsp" , "prettier"
+      "html-lsp", "css-lsp" , "prettier", "php-cs-fixed",
     },
   },
  },
