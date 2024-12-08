894f6c59b8dd283cab09b458a7b82a0e1b5ea165
Treast <vincent.riva@outlook.fr>
2024-12-08 09:16:54 +0100

    Setup PHPActor instead of Intelephense

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,10 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "intelephense" }
+local servers = { "html", "cssls", "phpactor" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -20,4 +20,14 @@ lspconfig.tsserver.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
+}
+
+-- phpactor
+lspconfig.phpactor.setup {
+  on_attach = on_attach,
+  init_options = {
+    ["code_transform.refactor.generate_accessor.prefix"] = "get",
+    ["code_transform.refactor.generate_accessor.upper_case_first"] = true,
+    ["code_transform.refactor.generate_mutator.fluent"] = true,
+  },
 }
